18a38c805fcd4b91fa160fa4f77bac798434136e
Koshkens <33977229+Koshkens@users.noreply.github.com>
2019-09-07 16:51:47 +0300

    add search

--- index.php
+++ index.php
@@ -9,18 +9,34 @@
 </head>
 <body>
     <div class="nav">
-        <input type="text" placeholder="   Введите поисковый запрос" class="nav__input">
-        <button class="nav__button">поиск</button>
+        <form method="post" action="">
+            <input type="text" name="search" placeholder="   Введите поисковый запрос" class="nav__input">
+            <input type="submit" class="nav__button" value="Поиск">
+        </form>
     </div>
         <ul class="">
         <?php
             include ('get_zapis.php');
+            $status =0;
             foreach($res as $zapis_full){
                 $zapis = $zapis_full["values"];
-                echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
-                                          '<span class="status">'.$zapis[3][0].'</span>'.
-                                          '<span class="otcenka">'.$zapis[4].'</span>'.
-                                          '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                $status =0;
+                if($zapis[4]==htmlspecialchars($_POST["search"])){
+                    echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
+                                            '<span class="status">'.$zapis[3][0].'</span>'.
+                                            '<span class="otcenka">'.$zapis[4].'</span>'.
+                                            '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                    $status++;
+                }elseif(htmlspecialchars($_POST["search"])==""){
+                    echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
+                                            '<span class="status">'.$zapis[3][0].'</span>'.
+                                            '<span class="otcenka">'.$zapis[4].'</span>'.
+                                            '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                    $status++;
+                }
+            }
+            if($status==0){
+               echo '<h2>Ничего не найденно</h2>';
             }
         ?>
         </ul>
