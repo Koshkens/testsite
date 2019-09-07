1bd7883770ef00e9f330c33f890fdcdcaae89ac1
Koshkens <33977229+Koshkens@users.noreply.github.com>
2019-09-08 00:55:58 +0300

    +css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Popkens</title>
+    <title>Панель</title>
 </head>
 <body>
     <div class="nav">
@@ -14,31 +14,33 @@
             <input type="submit" class="nav__button" value="Поиск">
         </form>
     </div>
+    <div class="content">
         <ul class="">
-        <?php
-            include ('get_zapis.php');
-            $status =0;
-            foreach($res as $zapis_full){
-                $zapis = $zapis_full["values"];
+            <?php
+                include ('get_zapis.php');
                 $status =0;
-                if($zapis[4]==htmlspecialchars($_POST["search"])){
-                    echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
-                                            '<span class="status">'.$zapis[3][0].'</span>'.
-                                            '<span class="otcenka">'.$zapis[4].'</span>'.
-                                            '<span class="text">'.$zapis[7].'</span>'.'</li>';
-                    $status++;
-                }elseif(htmlspecialchars($_POST["search"])==""){
-                    echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
-                                            '<span class="status">'.$zapis[3][0].'</span>'.
-                                            '<span class="otcenka">'.$zapis[4].'</span>'.
-                                            '<span class="text">'.$zapis[7].'</span>'.'</li>';
-                    $status++;
+                foreach($res as $zapis_full){
+                    $zapis = $zapis_full["values"];
+                    $status =0;
+                    if($zapis[4]==htmlspecialchars($_POST["search"])){
+                        echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
+                                                '<span class="status">'.$zapis[3][0].'</span>'.
+                                                '<span class="otcenka">'.$zapis[4].'</span>'.
+                                                '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                        $status++;
+                    }elseif(htmlspecialchars($_POST["search"])==""){
+                        echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
+                                                '<span class="status">'.$zapis[3][0].'</span>'.
+                                                '<span class="otcenka">'.$zapis[4].'</span>'.
+                                                '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                        $status++;
+                    }
                 }
-            }
-            if($status==0){
-               echo '<h2>Ничего не найденно</h2>';
-            }
-        ?>
+                if($status==0){
+                echo '<li><h2>Ничего не найденно</h2></li>';
+                }
+            ?>
         </ul>
+    </div>
 </body>
 </html>
