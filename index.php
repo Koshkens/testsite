50d0afa96d6dea792a8597919e6eeb09de20c894
Koshkens <33977229+Koshkens@users.noreply.github.com>
2019-09-09 00:10:49 +0300

    +css +ico

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -29,6 +30,10 @@
                         $status++;
                     }elseif(htmlspecialchars($_POST["search"])==""){
                         echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
+                                                '<span class="status">'.$zapis[3][0].'</span>'.
+                                                '<span class="otcenka">'.$zapis[4].'</span>'.
+                                                '<span class="text">'.$zapis[7].'</span>'.'</li>';
+                                                echo '<li class="zapis">'.'<span class="name_sotrudnik">'.$zapis[2][0][title].'</span>'.
                                                 '<span class="status">'.$zapis[3][0].'</span>'.
                                                 '<span class="otcenka">'.$zapis[4].'</span>'.
                                                 '<span class="text">'.$zapis[7].'</span>'.'</li>';
