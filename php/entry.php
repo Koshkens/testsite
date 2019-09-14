7d6d08d350733b7636bee4b158bd15ef1e07d223
Koshkens <33977229+Koshkens@users.noreply.github.com>
2019-09-14 15:50:46 +0300

    +change

--- php/entry.php
+++ php/entry.php
@@ -13,7 +13,6 @@
         $entry_id = $_GET['entry_id'];
         include ('../php/get_entry.php');
         $entry=$res["values"];
-        //var_dump($entry);
         
         $date = substr (date('Y-m-d H:i:s', strtotime($entry[2])),0,10);
                         
@@ -24,13 +23,13 @@
 
         switch ($entry[3][0]) {
             case '1':
-                $status = '<div class="green">Работаем</div>';
+                $status[0] = "checked";
                 break;
             case '2':
-                $status = '<div class="yellow">Переговоры</div>';
+                $status[1] = "checked";
                 break;
             case '3':
-                $status = '<div class="red">Холодный клиент</div>';
+                $status[2] = "checked";
                 break;
         }
         $title = $entry[4];
@@ -38,48 +37,48 @@
         $description = $entry[5];
 
         $LPR_name = $entry[7];
-        $segment = '';
+
         foreach($entry[8] as $num_of_segment){
             switch ($num_of_segment){
                 case '1':
-                    $segment = $segment.'<div class="ok"></div>Партнер ЭРА ';
+                    $segment[0]="checked";
                     break;
                 case '2':
-                    $segment = $segment.'<div class="ok"></div>Визажист ';
+                    $segment[1] = "checked";
                     break;
                 case '3':
-                    $segment = $segment.'<div class="ok"></div>Магазин офлайн ';
+                    $segment[2] = "checked";
                     break;
                 case '4':
-                    $segment = $segment.'<div class="ok"></div>ВК ';
+                    $segment[3] = "checked";
                     break;
                 case '5':
-                    $segment = $segment.'<div class="ok"></div>Insta ';
+                    $segment[4] = "checked";
                     break;
                 case '6':
-                    $segment = $segment.'<div class="ok"></div>300 руб ';
+                    $segment[5] = "checked";
                     break;
                 
             }
-        } 
+        }
     ?>
     <div class="entry">
         <div class="date">
             <span class="date__title, titleOf">Дата</span> <input class="date__input" type="date" name="date" value="<?php echo $date?>">
         </div>
         <div class="status"><span class="status__title, titleOf">Статус</span>
-            <input type="button" value="Холодный клиент" class="status__button">
-            <input type="button" value="Переговоры" class="status__button">
-            <input type="button" value="Работаем" class="status__button">
+            <span><input type="radio" name="status" value="Холодный клиент" class="status__button" <?php echo $status[0]?>>Работаем</span>
+            <span><input type="radio" name="status" value="Переговоры" class="status__button" <?php echo $status[1]?> >Переговоры</span>
+            <span><input type="radio" name="status" value="Работаем" class="status__button" <?php echo $status[2]?> >Холодный клиент</span>
         </div>
         <div class="segment">
             <span class="segment__title, titleOf">Сегмент</span>
-            <input type="checkbox" name="segment">Партнер ЭРА
-            <input type="checkbox" name="segment">Визажист
-            <input type="checkbox" name="segment">Магазин офлайн
-            <input type="checkbox" name="segment">ВК
-            <input type="checkbox" name="segment">Insta
-            <input type="checkbox" name="segment">300 руб
+            <input type="checkbox" name="segment" <?php echo $segment[0] ?>>Партнер ЭРА
+            <input type="checkbox" name="segment" <?php echo $segment[1] ?>>Визажист
+            <input type="checkbox" name="segment" <?php echo $segment[2] ?>>Магазин офлайн
+            <input type="checkbox" name="segment" <?php echo $segment[3] ?>>ВК
+            <input type="checkbox" name="segment" <?php echo $segment[4] ?>>Insta
+            <input type="checkbox" name="segment" <?php echo $segment[5] ?>>300 руб
         </div>
         <div class="title">
             <span class="title__title, titleOf">Название</span>
