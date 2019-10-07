aeeda1a0063f5a312fc94f60733e6a3dc3e28b1c
Koshkens <33977229+Koshkens@users.noreply.github.com>
2019-10-07 19:27:23 +0300

    edit sample doc

--- php_doc_page/document_1.php
+++ php_doc_page/document_1.php
@@ -2,20 +2,29 @@
 <html>
   <head>
     <style>
+    @page {
+   size: 7in 9.25in;
+   margin: 27mm 16mm 27mm 16mm;
+}
     body {
         height: 842px;
         width: 595px;
-        /* to centre page on screen*/
         margin-left: auto;
         margin-right: auto;
-        border:solid 1px;
         font-family: sans-serif;
         font-size: small;
         
         font-size: 16px;
     }
+    div{
+      text-indent: 1.5em;
+    }
     h1{
         text-align:center;
+    }
+    .main{
+        height:100%;
+        position:relative;
     }
     input{
         outline:none;
@@ -23,16 +32,29 @@
         font-family: sans-serif;
         font-size: 16px;
     }
+    .bottom{
+      position: absolute;
+      bottom: 10px;;
+    }
     </style>
   </head>
   <body>
-    <h1>Договор</h1>
-    <br>
-    <br>
-    <br>
-    <h2>Введение</h2>
-    Здраствуйте  <input type="text" onkeydown="this.style.width = ((this.value.length + 1) * 8) + 'px';" value="<?php echo $_POST["name"]?>">  ацу ацуа цду <b>адц</b> уадцо номер: <?php echo $_POST["number"]?> цуда цужа цу ажц уажц уажлц уал цуал цуоал цужл адццу адц 
-    ца цу ацдуо ацуа цу
-    
+    <div class="main">
+    <h1>Приглашение</h1>
+    <div>Уважаемый <input type="text" onkeydown="this.style.width = ((this.value.length + 1) * 8) + 'px';" value="<?php echo $_POST["name"]?>">,</div><br><br>
+
+    <div>Уважаемые дамы и господа! Мы рады пригласить Вас на выставку <input type="text" onkeydown="this.style.width = ((this.value.length + 1) * 8) + 'px';" value="" placeholder="название">, которая состоится в <input type="text" onkeydown="this.style.width = ((this.value.length + 1) * 8) + 'px';" value="" placeholder="место"> с 15.08.2013 г. по 19.08.2013 г.</div><br><br>
+
+    <div>Наша компания представит на выставке как традиционное оборудование для оперативной полиграфии, так и оборудование для специальных видов печати. </div><br><br>
+
+    <div>На нашем стенде мы представим:
+- товар 
+- товар 
+- товар</div> 
+<br><br>
+<div>Мы будем рады увидеть Вас в числе наших гостей и надеемся, что посещение этого мероприятия окажется для Вас всесторонне полезным.
+</div><br><br>
+<div class="bottom">С уважением,
+Петр Петров</div></div>
   </body>
 </html>
